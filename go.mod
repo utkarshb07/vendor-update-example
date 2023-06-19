80520a4b86859f0fb6c6d7bfb69e245d89a4d44b
utkarsh bairolia <utkarsh.bairolia@nutanix.com>
2023-06-19 07:46:56 +0000

    go package init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module vendor_update_example
+
+go 1.17
+
+require github.com/gofrs/uuid v4.4.0+incompatible
---
+++ main.go
@@ -0,0 +1,11 @@
+package vendor_update_example
+
+import (
+  "fmt"
+
+  _ "github.com/gofrs/uuid"
+)
+
+func X() {
+  fmt.Println("vim-go")
+}
